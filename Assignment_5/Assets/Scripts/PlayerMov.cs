8f663f0692c6b74909a21acba01fcf4b84508820
Josu Goitia <j.goitia@digipen.edu>
2020-10-07 12:51:09 +0200

    Basic PlayerMovement done

---
+++ Assignment_5/Assets/Scripts/PlayerMov.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMov : MonoBehaviour
+{
+    public float speed;                //Floating point variable to store the player's movement speed.
+
+    private Rigidbody2D rb ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 move = Vector2.zero; 
+    
+        if(Input.GetKey(KeyCode.W))
+        {
+            move.y += speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            move.y -= speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            move.x += speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            move.x -= speed;
+        }
+
+        rb.velocity = new Vector3(move.x, move.y, 0);
+    }
+}
