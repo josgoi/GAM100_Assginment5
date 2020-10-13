7362fc93acabcb40d72ce0183f882d0f239087e4
Jokin Martinez <jokin.martinez@digipen.edu>
2020-10-13 15:53:00 +0200

    Bullet Animation, Ball Health, Lose State and Lose UI done

--- Assignment_5/Assets/Scripts/Shooting.cs
+++ Assignment_5/Assets/Scripts/Shooting.cs
@@ -2,11 +2,20 @@
 using System.Collections.Generic;
 using System.Security.Cryptography;
 using UnityEngine;
+using TMPro;
 
 public class Shooting : MonoBehaviour
 {
     public Transform firePoint;
     public GameObject bulletPrefab;
+    public int BallsDestroyed = 0;
+    public TextMeshProUGUI EndText;
+    public GameObject EndTextobject;
+
+    void Start()
+    {
+        EndTextobject.SetActive(false);
+    }
 
     // Update is called once per frame
     void Update()
@@ -15,6 +24,16 @@ public class Shooting : MonoBehaviour
         {
             Shoot();
         }
+        if (BallsDestroyed > 10)
+        {
+            SetEndText();
+            EndTextobject.SetActive(true);
+        }
+    }
+
+    public void SetEndText()
+    {
+        EndText.text = "Final Score:" + GameObject.Find("GoalYellow").GetComponent<GoalYellow>().Score.ToString();
     }
 
     void Shoot()
