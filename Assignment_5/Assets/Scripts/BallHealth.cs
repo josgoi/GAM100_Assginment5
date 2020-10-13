7a596554a499810fa0d8aacfa5d79be45a65808c
Jokin Martinez <jokin.martinez@digipen.edu>
2020-10-13 15:53:00 +0200

    Bullet Animation, Ball Health, Lose State and Lose UI done

---
+++ Assignment_5/Assets/Scripts/BallHealth.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallHealth : MonoBehaviour
+{
+
+    int Health = 5;
+
+    void Update()
+    {
+        if(Health <= 0)
+        {
+            Destroy(gameObject);
+            GameObject.Find("Player").GetComponent<Shooting>().BallsDestroyed++;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D hitObj)
+    {
+        if (!hitObj.gameObject.name.Contains("Bullet"))
+        {
+            Health--;
+        }
+    }
+}
